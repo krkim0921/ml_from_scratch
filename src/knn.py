05e9f7ae9e1aab76c19e6047659fe6c36a4c18db
krkim0921 <chemestry100@gmail.com>
2021-06-01 13:18:48 +0900

    implemented KNN algorithms

--- src/knn.py
+++ src/knn.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from collections import Counter
 
 def euclidian_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
@@ -25,7 +25,25 @@ class KNN:
         return np.array(predicted_labels)
 
     def _predict(self, x):
+        '''
+        param: x --> 2d arrays
+        '''
+
+        #Calculate Distance 
+        # return a number: 1d array
         distances = [euclidian_distance(x, x_train) for x_train in self.X_train]
+
+        #Obtain K nearest samples, and labels
+        # return index
+        k_indices = np.argsort(distances)[:self.k]
+        #return labels
+        k_nearest_lables = [self.y_train[i] for i in k_indices]
+        # Majority Vote
+        # return List of Tuple
+        common_label = Counter(k_nearest_lables).most_common(1)
+        return common_label[0][0]
+
+
 
 
 
@@ -42,7 +60,12 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 777)
 
-    
+    knn_clf = KNN(k = 3)
+    knn_clf.fit(X_train, y_train)
+    predictions = knn_clf.predict(X_test)
+
+    acc = np.sum(predictions == y_test) / len(y_test)
+    print(acc)
     
 
     
